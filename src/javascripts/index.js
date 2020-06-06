be45ee9a5128118657355b0f5a31974e899aa13f
dzucconi <mail@damonzucconi.com>
2020-06-06 15:27:43 -0400

    Implements static/inlined build

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,77 +1,74 @@
 import { Calendar } from "calendar";
-import parameters from "queryparams";
 import length from "reading-time";
-import fps from "frame-interval";
-
-import { rand } from "./lib/rand";
+import { sample } from "./lib/rand";
 import { timeout } from "./lib/timeout";
 import { randomColor } from "./lib/randomColor";
 import { questions } from "./data/questions";
 
-window.parameters = parameters;
-
 const DOM = {
   app: document.getElementById("app"),
-  subtitles: document.getElementById("subtitles")
+  subtitles: document.getElementById("subtitles"),
 };
 
-const factor = rand(1, 10);
+const SCHEMES = [
+  { background: "black", color: "red" },
+  { background: "green", color: "gold" },
+  { background: "orange", color: "yellow" },
+  { background: "lime", color: "purple" },
+  { background: "red", color: "darkred" },
+  { background: "lightsalmon", color: "red" },
+  { background: "mistyrose", color: "azure" },
+  { background: "whitesmoke", color: "gainsboro" },
+  { background: "crimson", color: "deeppink" },
+  { background: "lemonchiffon", color: "plum" },
+  { background: "yellow", color: "black" },
+  { background: "springgreen", color: "olive" },
+  { background: "midnightblue", color: "blue" },
+];
 
-const PARAMS = parameters({
-  year: new Date().getFullYear() * factor,
-  background: "white",
-  color: "red",
-  play: false,
+const scheme = sample(SCHEMES);
+
+const PARAMS = {
+  year: 2016,
+  background: scheme.background,
+  color: scheme.color,
+  play: true,
   subtitles: false,
   fps: 12,
-  refreshIntervalSeconds: 3600
-});
-
-const STATE = {
-  year: PARAMS.year
+  refreshIntervalSeconds: 3600,
 };
 
-const render = year => {
+const STATE = {
+  year: PARAMS.year,
+};
+
+const calendar = new Calendar();
+
+const render = (year) => {
   if (year < 1970) STATE.year = 1970;
   if (year >= 199999) STATE.year = 1970;
 
-  const calendar = new Calendar();
-  const months = Array(12)
-    .fill(undefined)
-    .map((_, month) => calendar.monthDays(year, month));
+  let output = "";
+  let i, j, k;
+
+  for (i = 0; i < 12; i++) {
+    const month = calendar.monthDays(year, i);
+    output += '<div class="month">';
+    for (j = 0; j < month.length; j++) {
+      const week = month[j];
+      output += '<div class="week">';
+      for (k = 0; k < week.length; k++) {
+        const day = week[k];
+        output += `<div class='day ${day ? "" : "day--zero"}'>${day}</div>`;
+      }
+      output += "</div>";
+    }
+    output += "</div>";
+  }
 
   DOM.app.innerHTML = `
-    <div class='year'>
-      ${year}
-    </div>
-
-    <div id='months' class='months'>
-      ${months
-        .map(
-          weeks => `
-        <div class='month'>
-          ${weeks
-            .map(
-              days => `
-            <div class='week'>
-              ${days
-                .map(
-                  day => `
-                <div class='day ${day ? "" : "day--zero"}'>
-                  ${day}
-                </div>
-              `
-                )
-                .join("")}
-            </div>
-          `
-            )
-            .join("")}
-        </div>
-      `
-        )
-        .join("")}
-    </div>
+    <div class='year'>${year}</div>
+    <div id='months' class='months'>${output}</div>
   `;
 };
 
@@ -85,11 +82,14 @@ const init = () => {
     setTimeout(() => location.reload(), PARAMS.refreshIntervalSeconds * 1000);
   }
 
-  render(STATE.year);
+  const tick = () => {
+    render(STATE.year++);
+    setTimeout(() => {
+      requestAnimationFrame(tick);
+    }, 1000 / PARAMS.fps);
+  };
 
-  if (PARAMS.play) {
-    fps(requestAnimationFrame)(PARAMS.fps, () => render(STATE.year++))();
-  }
+  tick();
 
   if (PARAMS.subtitles) {
     questions.reduce((memo, question) => {
--- src/javascripts/lib/rand.js
+++ src/javascripts/lib/rand.js
@@ -1,1 +1,3 @@
 export const rand = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+export const sample = (xs) => xs[rand(0, xs.length - 1)];
